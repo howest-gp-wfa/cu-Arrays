b772cf25022c4ee69de0390cd400312d901e6162
Mileto Di Marco <mileto.di.marco@howest.be>
2021-09-13 14:06:33 +0200

    Fix conventions

--- js/script.js
+++ js/script.js
@@ -1,68 +1,68 @@
-"use strict";
+'use strict';
 
-window.addEventListener('load', Initieer);
+window.addEventListener('load', initialize);
 
 // Globale variabelen
-var txtIngave, divFeedback, boodschappenLijst, aantalProducten;
-var divArrayFuncties;
-var divArrayFuncties2;
-var divMulti;
-var txtVoornaam, txtNaam, txtGemeente;
-var adressen;
-var divDataWeergave;
+let txtIngave, divFeedback, boodschappenLijst, aantalProducten;
+let divArrayFuncties;
+let divArrayFuncties2;
+let divMulti;
+let txtVoornaam, txtNaam, txtGemeente;
+let adressen;
+let divDataWeergave;
 
-function Initieer() {
+function initialize() {
   // Lokale variabelen
   let btnArrayResult;
   let btnMultiVoegToe;
 
   // DOM elementen ophalen
-  txtIngave = document.getElementById('txtIngave');
-  txtVoornaam = document.getElementById('txtVoornaam');
-  txtNaam = document.getElementById('txtNaam');
-  txtGemeente = document.getElementById('txtGemeente');
-  divFeedback = document.getElementById('divFeedback');
-  divArrayFuncties = document.getElementById('divArrayFuncties');
-  divArrayFuncties2 = document.getElementById('divArrayFuncties2');
-  divMulti = document.getElementById('divMulti');
-  divDataWeergave = document.getElementById('divDataWeergave');
-  btnArrayResult = document.querySelector("#btnArrayResult");
-  btnMultiVoegToe = document.querySelector('#btnMultiVoegToe')
+  txtIngave = document.getElementById('ingave');
+  txtVoornaam = document.getElementById('voornaam');
+  txtNaam = document.getElementById('naam');
+  txtGemeente = document.getElementById('gemeente');
+  divFeedback = document.getElementById('feedback');
+  divArrayFuncties = document.getElementById('array-functies');
+  divArrayFuncties2 = document.getElementById('array-functies2');
+  divMulti = document.getElementById('multi');
+  divDataWeergave = document.getElementById('data-weergave');
+  btnArrayResult = document.querySelector("#array-result");
+  btnMultiVoegToe = document.querySelector('#multi-voegToe')
 
   // Eventlisteners toevoegen
-  btnArrayResult.addEventListener("click", VoegProductToeAanArray);
-  btnMultiVoegToe.addEventListener('click', VoegAdresToe)
+  btnArrayResult.addEventListener("click", voegProductToeAanArray);
+  btnMultiVoegToe.addEventListener('click', voegAdresToe)
 
   // Initialisatie van de variabelen
   aantalProducten = 0;
   boodschappenLijst = [];
 
   // Functies aanroepen
-  VulArrayFuncties();
-  VulArrayFuncties2();
-  VulAdressen();
-  ToonAdressen();
-  VulMetData();
+  vulArrayFuncties();
+  vulArrayFuncties2();
+  vulAdressen();
+  toonAdressen();
+  vulMetData();
 }
 
-function Compare(a,b,SorteerElement){
-  if (a[SorteerElement] < b[SorteerElement]){
+function Compare(a, b, SorteerElement) {
+  if (a[SorteerElement] < b[SorteerElement]) {
     return -1;
   }
-  if (a[SorteerElement] > b[SorteerElement]){
+  if (a[SorteerElement] > b[SorteerElement]) {
     return 1;
   }
   return 0;
 }
 
-function VulMetData(){
-  dataSitcom.sort((a,b,c) => Compare(a,b,'Rank'));
-  for(let dataObject in dataSitcom){
+function vulMetData() {
+  dataSitcom.sort((a, b, c) => Compare(a, b, 'Rank'));
+  for(let dataObject in dataSitcom) {
     divDataWeergave.appendChild(MaakDataDivisie(dataSitcom[dataObject]));
   }
 }
 
-function MaakDataDivisie(objectElement){
+function MaakDataDivisie(objectElement) {
   let DataDivisie = document.createElement('div');
   DataDivisie.className='dataDiv';
   DataDivisie.innerHTML = `Rank : ${objectElement.Rank} <br/> 
@@ -74,14 +74,14 @@ function MaakDataDivisie(objectElement){
 
 }
 
-function VulAdressen() {
+function vulAdressen() {
   adressen = [];
   adressen[0] = ["De Donder", "Walter", "Affligem"];
   adressen[1] = ["Crucke", "Koen", "Gent"];
   adressen[2] = ["Verhulst", "Gert", "Antwerpen"];
 }
 
-function ToonAdressen()
+function toonAdressen()
 {
   let samenvatting = "";
  
@@ -91,47 +91,45 @@ function ToonAdressen()
   divMulti.innerHTML = samenvatting;
 }
 
-function VoegAdresToe() {
+function voegAdresToe() {
   let voornaam = txtVoornaam.value;
   let naam = txtNaam.value;
   let gemeente = txtGemeente.value;
   let nieuwAdres = [naam, voornaam, gemeente];
   adressen.push(nieuwAdres);
-  ToonAdressen();
+  toonAdressen();
   txtVoornaam.value = "";
   txtNaam.value = "";
   txtGemeente.value = "";  
 }
 
 // Arrow functies in variabelen
-let ZoekEven = p => p%2==0;
-let Verdubbel = p => p*2;
+let ZoekEven = p => p%2 == 0;
+let Verdubbel = p => p * 2;
 
-function VulArrayFuncties2(){
+function vulArrayFuncties2() {
     let leeftijdArray = [15,31,17,19,52,51];
     let verdubbelLeeftijdArray;
     let result = '';
 
     verdubbelLeeftijdArray = leeftijdArray.map(Verdubbel);
     result = `  leeftijdArray => ${leeftijdArray}  <br /> 
-                leeftijdArray.every(p => p>=18) => ${leeftijdArray.every(p => (p>=18))} <br />
+                leeftijdArray.every(p => p>=18) => ${leeftijdArray.every(p => (p >= 18))} <br />
                 leeftijdArray.filter(ZoekEven) => ${leeftijdArray.filter(ZoekEven)} <br />
-                leeftijdArray.find(p=> p>=18) => ${leeftijdArray.find(p=> p>=18)} <br />
-                leeftijdArray.findIndex(p => p>=18) => ${leeftijdArray.findIndex(p=> p>=18)} <br/>
+                leeftijdArray.find(p=> p>=18) => ${leeftijdArray.find(p=> p >= 18)} <br />
+                leeftijdArray.findIndex(p => p>=18) => ${leeftijdArray.findIndex(p=> p >= 18)} <br/>
                 leeftijdArray.indexOf(19) => ${leeftijdArray.indexOf(19)} <br/>
                 leeftijdArray.includes(18) => ${leeftijdArray.includes(18)} <br/>
-                leeftijdArray.some(p => p<18) => ${leeftijdArray.some(p => p<18)} <br/>
+                leeftijdArray.some(p => p<18) => ${leeftijdArray.some(p => p < 18)} <br/>
                 verdubbelLeeftijdArray = leeftijdArray.map(Verdubbel) => ${verdubbelLeeftijdArray} <br />
-                leeftijdsArray.reduce ((a,b) => (a+b),0) => ${leeftijdArray.reduce((a,b) => (a+b),0)} <br />
+                leeftijdsArray.reduce ((a,b) => (a+b),0) => ${leeftijdArray.reduce((a, b) => (a + b), 0)} <br />
     `;
 
     divArrayFuncties2.innerHTML = result;
 
 }
 
-
-
-function VulArrayFuncties(){
+function vulArrayFuncties() {
   // Locale variabelen
   let fruitArray = ['Appel', 'Peer', 'Banaan', 'Kiwi','Meloen','Druif'];
   let groentenArray = ['Wortel', 'Sla', 'Tomaat','Komkommer'];
@@ -143,7 +141,7 @@ function VulArrayFuncties(){
               groentenArray.concat(fruitArray) => ${groentenArray.concat(fruitArray)} <br />
               fruitArray.reverse() => ${fruitArray.reverse()} <br />
               fruitArray.sort() => ${fruitArray.sort()} <br />
-              fruitArray.splice(1,2) => ${fruitArray.splice(1,2)} <br />
+              fruitArray.splice(1,2) => ${fruitArray.splice(1, 2)} <br />
               fruitArray na splice => ${fruitArray} <br />
               fuitArray.pop() => ${fruitArray.pop()} <br />
               fruitArray na pop => ${fruitArray} <br />
@@ -151,12 +149,10 @@ function VulArrayFuncties(){
               fruitArray na push => ${fruitArray} <br />
 
               `;
-
-
   divArrayFuncties.innerHTML = result; 
 }
 
-function VoegProductToeAanArray() {
+function voegProductToeAanArray() {
   let toeTeVoegenProduct = txtIngave.value;
 
   if (toeTeVoegenProduct.trim() != '') {
@@ -179,7 +175,7 @@ function GeefFeedBack() {
   }
   // Lussen met een for-in-lus
   result += '<u>For-in-lus</u> <br />';
-  for(let product in boodschappenLijst){
+  for(let product in boodschappenLijst) {
     result += `${boodschappenLijst[product]} <br />`;
   }
   result += `Aantal elementen in de Array boodschappenlijst => ${boodschappenLijst.length}`;
