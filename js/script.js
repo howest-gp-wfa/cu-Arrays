40aa72a3f36fc08a3ec296e16f64badb9d6eed43
maximlesy <maximlesy@gmail.com>
2021-09-15 20:38:26 +0200

    Clean up code

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ let divArrayFunctions;
 let divArrayFunctionsAdvanced;
 let divMulti;
 let inpFirstname, inpName, inpMunicipality;
-let adresses;
+let addresses;
 let divDataDisplay;
 
 function initialize() {
@@ -37,9 +37,21 @@ function initialize() {
   // Functions aanroepen
   fillArrayFunctions();
   fillArrayFunctionsAdvanced();
-  fillAdresses();
-  showAdresses();
-  fillWithData();
+  fillAddresses();
+  showAddresses();
+  fillSitcoms();
+}
+
+
+function fillSitcoms() {
+  
+  sitcomData.sort((a, b) => compare(a, b, "rank"));
+  
+  for (let i = 0; i < sitcomData.length; i++) {
+    const sitcomDiv = makeDataDivision(sitcomData[i]);
+    divDataDisplay.appendChild(sitcomDiv);
+  }
+  
 }
 
 function compare(a, b, propertyName) {
@@ -55,40 +67,28 @@ function compare(a, b, propertyName) {
   return 0;
 }
 
-function fillWithData() {
-
-  sitcomData.sort((a, b, c) => compare(a, b, "rank"));
-
-  for (let i = 0; i < sitcomData.length; i++) {
-    const sitcomDiv = makeDataDivision(sitcomData[i]);
-    divDataDisplay.appendChild(sitcomDiv);
-  }
-
-}
-
 function makeDataDivision(object) {
-  const dataDivision = document.createElement("div");
-  dataDivision.className = "dataDiv";
-  dataDivision.innerHTML = `Rank: ${object.rank} <br/> 
+  const sitcomDiv = document.createElement("div");
+  sitcomDiv.className = "sitcom";
+  sitcomDiv.innerHTML = `Rank: ${object.rank} <br/> 
                           SitcomName: ${object.sitcomName}  <br />
                           Years: ${object.years}`; 
 
-  return dataDivision;
+  return sitcomDiv;
 }
 
-function fillAdresses() {
-  adresses = [];
-  adresses[0] = ["De Donder", "Walter", "Affligem"];
-  adresses[1] = ["Crucke", "Koen", "Gent"];
-  adresses[2] = ["Verhulst", "Gert", "Antwerpen"];
+function fillAddresses() {
+  addresses = [];
+  addresses[0] = ["De Donder", "Walter", "Affligem"];
+  addresses[1] = ["Crucke", "Koen", "Gent"];
+  addresses[2] = ["Verhulst", "Gert", "Antwerpen"];
 }
 
-function showAdresses()
-{
+function showAddresses() {
   let summary = "";
  
-  for (let i = 0; i < adresses.length; i++) {
-      summary += `${adresses[i][1]} ${adresses[i][0]} - ${adresses[i][2]}<br />` ;      
+  for (let i = 0; i < addresses.length; i++) {
+      summary += `${addresses[i][1]} ${addresses[i][0]} - ${addresses[i][2]}<br />` ;      
   }
 
   divMulti.innerHTML = summary;
@@ -101,25 +101,23 @@ function addAdress() {
   const municipality = inpMunicipality.value;
   const newAdress = [name, firstname, municipality];
 
-  adresses.push(newAdress);
-  showAdresses();
+  addresses.push(newAdress);
+  showAddresses();
 
-  fillWithData.value = "";
+  fillSitcoms.value = "";
   inpName.value = "";
   inpMunicipality.value = "";
 }
 
 // Arrow Functions in variabelen
-let findEven = p => p % 2 === 0;
-let double = p => p * 2;
+const findEven = p => p % 2 === 0;
+const double = p => p * 2;
 
 function fillArrayFunctionsAdvanced() {
 
   const ageArray = [15, 31, 17 ,19 ,52 ,51];
-  let doubleAgeArray;
+  const doubleAgeArray = ageArray.map(double);
   let result = "";
-
-  doubleAgeArray = ageArray.map(double);
 
   result = `  ageArray => ${ageArray}  <br /> 
               ageArray.every(p => p >= 18) => ${ageArray.every(p => (p >= 18))} <br />
@@ -139,12 +137,12 @@ function fillArrayFunctionsAdvanced() {
 
 function fillArrayFunctions() {
   // local vars
-  const fruits = ['Appel', 'Peer', 'Banaan', 'Kiwi','Meloen','Druif'];
-  const vegetables = ['Wortel', 'Sla', 'Tomaat','Komkommer'];
-  let result = '<h3> Overzicht Funties </h3>';
+  const fruits = ["Appel", "Peer", "Banaan", "Kiwi","Meloen","Druif"];
+  const vegetables = ["Wortel", "Sla", "Tomaat","Komkommer"];
+  let result = "<h3> Overzicht Funties </h3>";
 
   result += ` fruits.join() => ${fruits.join()} <br />
-              vegetables.join(-) => ${vegetables.join('-')} <br />
+              vegetables.join(-) => ${vegetables.join("-")} <br />
               fruits => ${fruits} <br />
               vegetables.concat(fruits) => ${vegetables.concat(fruits)} <br />
               fruits.reverse() => ${fruits.reverse()} <br />
@@ -153,7 +151,7 @@ function fillArrayFunctions() {
               fruits na splice => ${fruits} <br />
               fruits.pop() => ${fruits.pop()} <br />
               fruits na pop => ${fruits} <br />
-              fruits.push('Aardbei') => ${fruits.push('Aardbei')} <br/>
+              fruits.push("Aardbei") => ${fruits.push("Aardbei")} <br/>
               fruits na push => ${fruits} <br />`;
 
   divArrayFunctions.innerHTML = result; 
@@ -166,18 +164,18 @@ function addProductToArray() {
 
     errandList[numberOfProducts] = productToAdd;
     numberOfProducts++;
-    inpInput.value = '';
+    inpInput.value = "";
     giveFeedback();
   }
   else {
-    console.warn('Geen waarde in adress');
+    console.warn("Geen waarde ingegeven");
   }
 }
 
 function giveFeedback() {
 
-  let result = '<h3> Array Elementen </h3>';
-  result += '<u>For-lus</u> <br />'; 
+  let result = "<h3> Array Elementen </h3>";
+  result += "<u>For-lus</u> <br />"; 
 
   for (let i = 0; i < errandList.length; i++) {
     result += `${errandList[i]} <br />`;
